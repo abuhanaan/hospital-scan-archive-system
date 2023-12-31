9a99aaa74d5be16ec0d8d7febb4b4b240f2ca49e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-31 04:50:06 +0100

    feat: Add search & pagination with style

--- hospital-scan-archive-system-FE/src/components/AddButton.jsx
+++ hospital-scan-archive-system-FE/src/components/AddButton.jsx
@@ -2,7 +2,7 @@ import { HiOutlinePlus } from "react-icons/hi";
 
 function AddButton({children}) {
     return (
-        <button className="bg-[#477BFF] text-white">
+        <button className=" flex items-center gap-1 py-3 px-4 rounded-md bg-[#477BFF] text-white">
             <HiOutlinePlus size={20} className="stroke-2" />
             {children}
         </button>
--- hospital-scan-archive-system-FE/src/pages/DoctorsList.jsx
+++ hospital-scan-archive-system-FE/src/pages/DoctorsList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaUserDoctor } from "react-icons/fa6";
+import { MdOutlineEdit, MdDeleteOutline } from "react-icons/md";
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
+
+import { IoSearch } from "react-icons/io5";
+
 import AddButton from '../components/AddButton';
 
 const DoctorsList = () => {
     const navigate = useNavigate();
     const doctors = [
-        { id: 1, firstName: 'Sodiq', lastName: 'Ishola', email: 'example123@gmail.com', specialty: 'Gynaecologist', role: 'doctor', img: <FaUserDoctor size={32} /> },
-        { id: 2, firstName: 'Hamzat', lastName: 'Turawa', email: 'example123@gmail.com', specialty: 'Neurologist', role: 'doctor', img: <FaUserDoctor size={32} /> },
-        { id: 3, firstName: 'Halimah', lastName: 'Salis', email: 'example123@gmail.com', specialty: 'Opthalmologist', role: 'doctor', img: <FaUserDoctor size={32} /> },
-        { id: 4, firstName: 'Zainab', lastName: 'Musa', email: 'example123@gmail.com', specialty: 'Dermatologist', role: 'doctor', img: <FaUserDoctor size={32} /> },
+        { id: 1, firstName: 'Sodiq', lastName: 'Ishola', email: 'example123@gmail.com', specialty: 'Gynaecologist', role: 'doctor', img: 'https://p7.hiclipart.com/preview/14/65/239/ico-avatar-scalable-vector-graphics-icon-doctor-with-stethoscope.jpg' },
+        { id: 2, firstName: 'Hamzat', lastName: 'Turawa', email: 'example123@gmail.com', specialty: 'Neurologist', role: 'doctor', img: 'https://cdn.imgbin.com/2/21/3/imgbin-computer-icons-doctor-of-medicine-health-care-physician-health-f0nPVnd5XSZmZra6C8BRi8kU4.jpg' },
+        { id: 3, firstName: 'Halimah', lastName: 'Salis', email: 'example123@gmail.com', specialty: 'Opthalmologist', role: 'doctor', img: 'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/7525866/hijab-doctor-clipart-md.png' },
+        { id: 4, firstName: 'Zainab', lastName: 'Musa', email: 'example123@gmail.com', specialty: 'Dermatologist', role: 'doctor', img: 'https://www.clipartmax.com/png/middle/244-2445799_doctor-female-doctor-icon.png' },
     ];
 
     function viewDoctor(scanId) {
@@ -17,7 +22,7 @@ const DoctorsList = () => {
     }
 
     return (
-        <div className="mt-6 min-h-screen w-full">
+        <div className="mt-6 min-h-screen w-full font-poppins">
             <div className="flex justify-between items-center mb-6 w-full">
                 <h1 className="text-[#15254C] text-2xl font-bold">Doctors List</h1>
                 <AddButton>Add New</AddButton>
@@ -28,57 +33,90 @@ const DoctorsList = () => {
                     doctors?.length === 0 ?
                         <EmptySearch headers={['Posts', 'Published']} />
                         :
-                        <div className="overflow-auto">
-                            <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                                <thead className="bg-gray-100 dark:bg-gray-700">
-                                    <tr className="">
-                                        <th scope="col" className="th">
-                                            Image
-                                        </th>
-                                        <th scope="col" className="th">
-                                            First Name
-                                        </th>
-                                        <th scope="col" className="th">
-                                            Last Name
-                                        </th>
-                                        <th scope="col" className="th">
-                                            Email
-                                        </th>
-                                        <th scope="col" className="th">
-                                            Specialty
-                                        </th>
-                                        <th scope="col" className="th">
-                                            Role
-                                        </th>
-                                        <th scope="col" className="th">
-                                            Action
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                                    {
-                                        doctors.map(doctor => (
-                                            <tr key={doctor.id} onClick={() => viewDoctor(doctor.id)} className="hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
-                                                <td className="table-data">
-                                                    {doctor.img}
-                                                </td>
-                                                <td className="table-data">{doctor.firstName}</td>
-                                                <td className="table-data">{doctor.lastName}</td>
-                                                <td className="table-data">{doctor.email}</td>
-                                                <td className="table-data">{doctor.specialty}</td>
-                                                <td className="table-data">{doctor.role}</td>
-                                                <td className="py-4 px-6 text-sm font-medium whitespace-nowrap flex items-center justify-center gap-1">
-                                                    <button className="">+</button>
-                                                    <button className="">-</button>
-                                                    <button className="">/</button>
-                                                </td>
-                                            </tr>
+                        <div className="flex flex-col">
+                            <div className="mt-6 mb-4 md:flex md:items-center md:justify-between">
+                                <div className="relative flex items-center mt-4 md:mt-0">
+                                    <span className="absolute w-5 h-5 mx-3 text-gray-400 dark:text-gray-600">
+                                        <IoSearch size={18} />
+                                    </span>
 
-                                        ))
-                                    }
+                                    <input type="text" placeholder="Search" className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                </div>
+                            </div>
 
-                                </tbody>
-                            </table>
+                            <div className="overflow-auto">
+                                <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                                    <thead className="bg-gray-100 dark:bg-gray-700">
+                                        <tr className="">
+                                            <th scope="col" className="th">
+                                                Profile Image
+                                            </th>
+                                            <th scope="col" className="th">
+                                                First Name
+                                            </th>
+                                            <th scope="col" className="th">
+                                                Last Name
+                                            </th>
+                                            <th scope="col" className="th">
+                                                Email
+                                            </th>
+                                            <th scope="col" className="th">
+                                                Specialty
+                                            </th>
+                                            <th scope="col" className="th">
+                                                Role
+                                            </th>
+                                            <th scope="col" className="th">
+                                                Action
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                        {
+                                            doctors.map(doctor => (
+                                                <tr key={doctor.id} onClick={() => viewDoctor(doctor.id)} className="hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+                                                    <td className="table-data">
+                                                        <img src={doctor.img} className='w-10 h-10 rounded-full' alt={`Dr. ${doctor.firstName} ${doctor.lastName}`} />
+                                                    </td>
+                                                    <td className="table-data">{doctor.firstName}</td>
+                                                    <td className="table-data">{doctor.lastName}</td>
+                                                    <td className="table-data">{doctor.email}</td>
+                                                    <td className="table-data">{doctor.specialty}</td>
+                                                    <td className="table-data">{doctor.role}</td>
+                                                    <td className="py-4 px-6 whitespace-nowrap flex items-center justify-center gap-1">
+                                                        <a href="#" className="text-grey-lighter py-1 px-1 rounded-md bg-blue-600 hover:bg-blue-700"><MdOutlineEdit size={20} color='white' /></a>
+                                                        <a href="#" className="text-grey-lighter py-1 px-1 rounded-md bg-red-600 hover:bg-red-700"><MdDeleteOutline size={20} color='white' /></a>
+                                                    </td>
+                                                </tr>
+
+                                            ))
+                                        }
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div className="mt-6 sm:flex sm:items-center sm:justify-between ">
+                                <div className="text-sm text-gray-500 dark:text-gray-400">
+                                    Page <span className="font-medium text-gray-700 dark:text-gray-100">1 of 10</span>
+                                </div>
+
+                                <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
+                                    <a href="#" className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+                                        <FaArrowLeftLong size={18} />
+                                        <span>
+                                            previous
+                                        </span>
+                                    </a>
+
+                                    <a href="#" className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+                                        <span>
+                                            Next
+                                        </span>
+                                        <FaArrowRightLong size={18} />
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                 }
             </div>
