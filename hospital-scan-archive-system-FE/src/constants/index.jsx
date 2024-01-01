40dd1391b16e372e3431eb214715894e08d3cb84
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-01-01 13:56:35 +0100

    feat: Create a file that stores data

---
+++ hospital-scan-archive-system-FE/src/constants/index.jsx
@@ -0,0 +1,179 @@
+import { FaUserDoctor, FaUserInjured, FaFileMedical, FaStethoscope } from "react-icons/fa6";
+
+export const scans = [
+    {
+        scanId: '1',
+        scanSymptoms: 'Chest pains',
+        scanDiagnosis: 'Pregnancy',
+        scanType: 'Ultrasound',
+        scanDate: '8/10/2023',
+        scanUrl: 'https://chest-pain-sandra.zip',
+        userId: 1,
+        userName: 'Sodiq Ishola',
+        userEmail: 'example123@gmail.com',
+        userSpecialty: 'Gynaecologist',
+        userRole: 'doctor',
+        userImg: 'https://p7.hiclipart.com/preview/14/65/239/ico-avatar-scalable-vector-graphics-icon-doctor-with-stethoscope.jpg',
+        patientId: 1,
+        patientName: 'Sandra Grace',
+        patientPhoneNumber: '08078903425',
+        patientNextOfKinName: 'Aliyu Rasheed',
+        patientNextOfKinPhone: '08054321490',
+        patientNextOfKinRelationship: 'husband',
+        patientAddress: '18, Ajanlekoko street, Lagos.',
+        patientAge: '32'
+    },
+    {
+        scanId: '2',
+        scanSymptoms: 'Chest pains',
+        scanDiagnosis: 'Pregnancy',
+        scanType: 'Ultrasound',
+        scanDate: '8/10/2023',
+        scanUrl: 'https://chest-pain-sandra.zip',
+        userId: 1,
+        userName: 'Sodiq Ishola',
+        userEmail: 'example123@gmail.com',
+        userSpecialty: 'Gynaecologist',
+        userRole: 'doctor',
+        userImg: 'https://p7.hiclipart.com/preview/14/65/239/ico-avatar-scalable-vector-graphics-icon-doctor-with-stethoscope.jpg',
+        patientId: 1,
+        patientName: 'Sandra Grace',
+        patientPhoneNumber: '08078903425',
+        patientNextOfKinName: 'Aliyu Rasheed',
+        patientNextOfKinPhone: '08054321490',
+        patientNextOfKinRelationship: 'husband',
+        patientAddress: '18, Ajanlekoko street, Lagos.',
+        patientAge: '32'
+    },
+    {
+        scanId: '3',
+        scanSymptoms: 'Chest pains',
+        scanDiagnosis: 'Pregnancy',
+        scanType: 'Ultrasound',
+        scanDate: '8/10/2023',
+        scanUrl: 'https://chest-pain-sandra.zip',
+        userId: 1,
+        userName: 'Sodiq Ishola',
+        userEmail: 'example123@gmail.com',
+        userSpecialty: 'Gynaecologist',
+        userRole: 'doctor',
+        userImg: 'https://p7.hiclipart.com/preview/14/65/239/ico-avatar-scalable-vector-graphics-icon-doctor-with-stethoscope.jpg',
+        patientId: 1,
+        patientName: 'Sandra Grace',
+        patientPhoneNumber: '08078903425',
+        patientNextOfKinName: 'Aliyu Rasheed',
+        patientNextOfKinPhone: '08054321490',
+        patientNextOfKinRelationship: 'husband',
+        patientAddress: '18, Ajanlekoko street, Lagos.',
+        patientAge: '32'
+    },
+    {
+        scanId: '4',
+        scanSymptoms: 'Chest pains',
+        scanDiagnosis: 'Pregnancy',
+        scanType: 'Ultrasound',
+        scanDate: '8/10/2023',
+        scanUrl: 'https://chest-pain-sandra.zip',
+        userId: 1,
+        userName: 'Sodiq Ishola',
+        userEmail: 'example123@gmail.com',
+        userSpecialty: 'Gynaecologist',
+        userRole: 'doctor',
+        userImg: 'https://p7.hiclipart.com/preview/14/65/239/ico-avatar-scalable-vector-graphics-icon-doctor-with-stethoscope.jpg',
+        patientId: 1,
+        patientName: 'Sandra Grace',
+        patientPhoneNumber: '08078903425',
+        patientNextOfKinName: 'Aliyu Rasheed',
+        patientNextOfKinPhone: '08054321490',
+        patientNextOfKinRelationship: 'husband',
+        patientAddress: '18, Ajanlekoko street, Lagos.',
+        patientAge: '32'
+    },
+
+];
+
+export const users = [
+    {
+        id: 1,
+        firstName: 'Sodiq',
+        lastName: 'Ishola',
+        email: 'example123@gmail.com',
+        specialty: 'Gynaecologist',
+        role: 'doctor',
+        img: 'https://p7.hiclipart.com/preview/14/65/239/ico-avatar-scalable-vector-graphics-icon-doctor-with-stethoscope.jpg'
+    },
+    {
+        id: 2,
+        firstName: 'Hamzat',
+        lastName: 'Turawa',
+        email: 'example123@gmail.com',
+        specialty: 'Neurologist', role: 'doctor',
+        img: 'https://cdn.imgbin.com/2/21/3/imgbin-computer-icons-doctor-of-medicine-health-care-physician-health-f0nPVnd5XSZmZra6C8BRi8kU4.jpg'
+    },
+    {
+        id: 3,
+        firstName: 'Halimah',
+        lastName: 'Salis',
+        email: 'example123@gmail.com',
+        specialty: 'Opthalmologist',
+        role: 'doctor',
+        img: 'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/7525866/hijab-doctor-clipart-md.png'
+    },
+    {
+        id: 4,
+        firstName: 'Zainab',
+        lastName: 'Musa',
+        email: 'example123@gmail.com',
+        specialty: 'Dermatologist',
+        role: 'nurse',
+        img: 'https://www.clipartmax.com/png/middle/244-2445799_doctor-female-doctor-icon.png'
+    },
+];
+
+export const patients = [
+    {
+        id: 1,
+        firstName: 'Sandra',
+        lastName: 'Grace',
+        phoneNumber: '08078903425',
+        address: '18, Ajanlekoko street, Lagos.',
+        age: '32',
+        nextOfKinName: 'Aliyu Rasheed',
+        nextOfKinRelationship: 'husband',
+        nextOfKinPhone: '08054125690',
+    },
+    {
+        id: 2,
+        firstName: 'Yusuf',
+        lastName: 'Tajudeen',
+        phoneNumber: '07054908745',
+        nextOfKin: 'Aliyu Rasheed',
+        address: '18, Ajanlekoko street, Lagos.',
+        age: '54'
+    },
+    {
+        id: 3,
+        firstName: 'Hikmah',
+        lastName: 'Boladale',
+        phoneNumber: '07012308745',
+        nextOfKin: 'Aliyu Rasheed',
+        address: '18, Ajanlekoko street, Lagos.',
+        age: '41'
+    },
+    {
+        id: 4,
+        firstName: 'Sandra',
+        lastName: 'Ali',
+        phoneNumber: '07054901235',
+        nextOfKin: 'Aliyu Rasheed',
+        address: '18, Ajanlekoko street, Lagos.',
+        age: '22'
+    }
+];
+
+export const dashboardCardsInfo = [
+    { id: 'doctors', title: 'Total Doctors', count: 2, icon: <FaUserDoctor size={20} /> },
+    { id: 'patients', title: 'Total Patients', count: 6, icon: <FaUserInjured size={20} /> },
+    { id: 'scans', title: 'Total Scans', count: 10, icon: <FaFileMedical size={20} /> },
+    { id: 'diagnosis', title: 'Total Diagnosis', count: 289, icon: <FaStethoscope size={20} /> },
+];
