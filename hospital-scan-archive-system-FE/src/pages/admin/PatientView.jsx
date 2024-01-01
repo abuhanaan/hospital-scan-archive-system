2c7f65b1454fb60eff4df30e7f870b0937f30e80
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-01-01 04:34:48 +0100

    feat: Create ScansList Page

--- hospital-scan-archive-system-FE/src/pages/admin/PatientView.jsx
+++ hospital-scan-archive-system-FE/src/pages/admin/PatientView.jsx
@@ -107,7 +107,6 @@ const PatientView = () => {
 
                                                 ))
                                             }
-
                                         </tbody>
                                     </table>
                                 </div>
