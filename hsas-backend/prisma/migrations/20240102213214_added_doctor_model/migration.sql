9f295875f1b97d35ca8b89f8043f9c1f6a806701
abuhanaan <adewunmi99@gmail.com>
2024-01-02 22:34:24 +0100

    Added Doctor Model having 1:1 rel with User

---
+++ hsas-backend/prisma/migrations/20240102213214_added_doctor_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `Doctor` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `phoneNumber` VARCHAR(191) NOT NULL,
+    `speciality` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` INTEGER NULL,
+
+    UNIQUE INDEX `Doctor_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Doctor` ADD CONSTRAINT `Doctor_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
