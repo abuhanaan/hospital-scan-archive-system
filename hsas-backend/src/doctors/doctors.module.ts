37ae36270a6b164dd2be711317195af63dad96b1
abuhanaan <adewunmi99@gmail.com>
2024-01-04 06:53:03 +0100

    Broke Circular Dependency Between User And Doctor Modules

--- hsas-backend/src/doctors/doctors.module.ts
+++ hsas-backend/src/doctors/doctors.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { DoctorsService } from './doctors.service';
 import { DoctorsController } from './doctors.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
@@ -7,7 +7,10 @@ import { UsersModule } from 'src/users/users.module';
 @Module({
   controllers: [DoctorsController],
   providers: [DoctorsService],
-  imports: [PrismaModule, UsersModule],
+  imports: [
+    PrismaModule,
+    forwardRef(() => UsersModule), // breaking circular dependency with UsersModule
+  ],
   exports: [DoctorsService],
 })
 export class DoctorsModule {}
--- hsas-backend/src/users/users.module.ts
+++ hsas-backend/src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
@@ -7,7 +7,11 @@ import { DoctorsModule } from 'src/doctors/doctors.module';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [PrismaModule, DoctorsModule],
+  imports: [
+    PrismaModule,
+    // breaking circular dependency with DoctorsModule
+    forwardRef(() => DoctorsModule),
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
