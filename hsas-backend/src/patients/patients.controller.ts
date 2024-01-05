551bc4ced4cb8391b0555dd5eef863730ac9f07a
abuhanaan <adewunmi99@gmail.com>
2024-01-05 10:38:10 +0100

    Implemented Auth on Patient Handlers

--- hsas-backend/src/patients/patients.controller.ts
+++ hsas-backend/src/patients/patients.controller.ts
@@ -7,12 +7,20 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { UpdatePatientDto } from './dto/update-patient.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PatientEntity } from './entities/patient.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { AdminJwtAuthGuard } from 'src/auth/guards/admin-auth.guards';
 
 @ApiTags('patients')
 @Controller('patients')
@@ -20,6 +28,8 @@ export class PatientsController {
   constructor(private readonly patientsService: PatientsService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiCreatedResponse({ type: PatientEntity })
   async create(@Body() createPatientDto: CreatePatientDto) {
     const patient = await this.patientsService.create(createPatientDto);
@@ -27,6 +37,8 @@ export class PatientsController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: PatientEntity, isArray: true })
   async findAll() {
     const patients = await this.patientsService.findAll();
@@ -34,6 +46,8 @@ export class PatientsController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: PatientEntity })
   async findOne(@Param('id', ParseIntPipe) id: number) {
     const patient = await this.patientsService.findOne(id);
@@ -41,6 +55,8 @@ export class PatientsController {
   }
 
   @Get('/scans/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: PatientEntity })
   async fetchPatientWithScans(@Param('id', ParseIntPipe) id: number) {
     const patient = await this.patientsService.fetchPatientWithScans(id);
@@ -48,6 +64,8 @@ export class PatientsController {
   }
 
   @Get('/doctors/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: PatientEntity })
   async fetchPatientWithDoctors(@Param('id', ParseIntPipe) id: number) {
     const patient = await this.patientsService.fetchPatientWithDoctors(id);
@@ -55,6 +73,8 @@ export class PatientsController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: PatientEntity })
   async update(
     @Param('id', ParseIntPipe) id: number,
@@ -65,6 +85,8 @@ export class PatientsController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, AdminJwtAuthGuard)
+  @ApiBearerAuth()
   async remove(@Param('id') id: string) {
     const patient = await this.patientsService.remove(+id);
     return new PatientEntity(patient);
