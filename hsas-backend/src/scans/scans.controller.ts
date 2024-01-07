fc5c8c7028e046d2a88e762b8905f6be55b910d2
abuhanaan <adewunmi99@gmail.com>
2024-01-07 11:44:39 +0100

    Added auth to scan controllers

--- hsas-backend/src/scans/scans.controller.ts
+++ hsas-backend/src/scans/scans.controller.ts
@@ -12,11 +12,15 @@ import {
   ValidationPipe,
   UploadedFile,
   ParseFilePipe,
+  UseGuards,
+  Req,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { ScansService } from './scans.service';
 import { CreateScanDto } from './dto/create-scan.dto';
 import { UpdateScanDto } from './dto/update-scan.dto';
 import {
+  ApiBearerAuth,
   ApiConsumes,
   ApiCreatedResponse,
   ApiOkResponse,
@@ -24,6 +28,10 @@ import {
 } from '@nestjs/swagger';
 import { ScanEntity } from './entities/scan.entity';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { AuthenticatedRequest } from 'src/utils/interfaces/authRequest.interface';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { AdminJwtAuthGuard } from 'src/auth/guards/admin-auth.guards';
 
 @Controller('scans')
 @ApiTags('scans')
@@ -31,15 +39,25 @@ export class ScansController {
   constructor(private readonly scansService: ScansService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiConsumes('multipart/form-data')
   @UsePipes(new ValidationPipe())
   @UseInterceptors(FileInterceptor('file'))
   @ApiCreatedResponse({ type: ScanEntity })
   async create(
+    @Req() request: AuthenticatedRequest,
     @Body() createScanDto: CreateScanDto,
     @UploadedFile(new ParseFilePipe({ validators: [] }))
     file: Express.Multer.File,
   ) {
+    const user = request.user as UserEntity;
+    if (user.role === 'doctor' && createScanDto.doctorId !== user.id) {
+      throw new UnauthorizedException({
+        message: `You can only upload scans that you prescribe`,
+        error: 'Unauthorised operation',
+      });
+    }
     const scan = await this.scansService.create(
       createScanDto,
       file.originalname,
@@ -49,6 +67,8 @@ export class ScansController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: ScanEntity, isArray: true })
   async findAll() {
     const scans = await this.scansService.findAll();
@@ -56,6 +76,8 @@ export class ScansController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: ScanEntity })
   async findOne(@Param('id', ParseIntPipe) id: number) {
     const scan = await this.scansService.findOne(id);
@@ -63,16 +85,32 @@ export class ScansController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: ScanEntity })
   async update(
+    @Req() request: AuthenticatedRequest,
     @Param('id', ParseIntPipe) id: number,
     @Body() updateScanDto: UpdateScanDto,
   ) {
+    const user = request.user as UserEntity;
+    if (
+      updateScanDto.doctorId &&
+      user.role === 'doctor' &&
+      updateScanDto.doctorId !== user.id
+    ) {
+      throw new UnauthorizedException({
+        message: `You can only update scans that you prescribe`,
+        error: 'Unauthorised operation',
+      });
+    }
     const scan = await this.scansService.update(id, updateScanDto);
     return new ScanEntity(scan);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, AdminJwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: ScanEntity })
   async remove(@Param('id', ParseIntPipe) id: number) {
     const scan = await this.scansService.remove(id);
