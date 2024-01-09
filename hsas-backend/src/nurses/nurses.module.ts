c2071802570721be34d0ab2d27e1acc5fae89abd
abuhanaan <adewunmi99@gmail.com>
2024-01-09 04:31:20 +0100

    Fix: exported NurseService

--- hsas-backend/src/nurses/nurses.module.ts
+++ hsas-backend/src/nurses/nurses.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   controllers: [NursesController],
   providers: [NursesService],
   imports: [PrismaModule],
+  exports: [NursesService],
 })
 export class NursesModule {}
