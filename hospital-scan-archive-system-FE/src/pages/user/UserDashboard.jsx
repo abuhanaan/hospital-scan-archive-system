6b867b4eb146fb46a092fecd07881d18ae2c983f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-01-14 03:00:07 +0100

    feat: Integrate user dahboard & pages with API

--- hospital-scan-archive-system-FE/src/pages/user/UserDashboard.jsx
+++ hospital-scan-archive-system-FE/src/pages/user/UserDashboard.jsx
@@ -5,119 +5,153 @@ import { doctorCardsInfo, scans } from '../../constants';
 import { Link, useLoaderData } from "react-router-dom";
 import { HiUser } from 'react-icons/hi';
 import { MdOutlineEdit } from 'react-icons/md';
+import { FaUserDoctor, FaUserInjured, FaFileMedical, FaStethoscope, FaUserNurse } from "react-icons/fa6";
+import { requireAuth } from '../../utils';
+import { getUserHomePageData } from '../../api';
 
-export async function loader() {
-    const user = JSON.parse(localStorage.getItem('user'));
-    const userScans = scans.filter(scan => scan.userId === user.id);
-    const cardsInfo = doctorCardsInfo;
-    const data = [cardsInfo, userScans, user];
+export async function loader({ request }) {
+    await requireAuth(request);
 
-    return data;
+    const data = await getUserHomePageData(request);
+
+    if (data.message || data.error) {
+        return {
+            error: `${data.error}: ${data.message}`
+        };
+    }
+
+    const doctorData = !data.user && data;
+    const nurseData = !data.scanCount && data;
+
+    // return [dashboardCardsInfo, data.recentScans, user];
+    if (doctorData) {
+        return doctorData;
+    }
+    
+    return nurseData;
 }
 
 const UserDashboard = () => {
-    const [cardsData, scansData, user] = useLoaderData();
+    const data = useLoaderData();
+    
+    const user = JSON.parse(localStorage.getItem('user'));
+    const doctorData = !data.error && !data.user && data;
+    const nurseData = !data.error && data;
+    const errorData = data.error && data;
 
-    const title = user.role === 'doctor' ? 'Dr.' : '';
+    const dashboardCardsInfo = [
+        { id: 'patients', title: 'Total Patients', count: doctorData?.patientCount, icon: <FaUserInjured size={20} /> },
+        { id: 'scans', title: 'Total Scans', count: doctorData?.scanCount, icon: <FaFileMedical size={20} /> },
+    ];
+
+    const title = user.role === 'doctor' ? 'Doctor' : 'Nurse';
 
     return (
         <section className="overflow-x-auto font-poppins">
-            <Welcome person={`${title} ${user.firstName}`} />
+            <Welcome person={`${title}`} />
 
             {
-                user.role === 'doctor' ?
-                    <>
-                        <div className="grid grid-cols-1 gap-4 ss:grid-cols-2 sm:grid-cols-3 mt-6">
-                            {
-                                cardsData.map((data, index) => {
-
-                                    return (
-                                        <OverviewCard key={data.id} count={data.count} title={data.title} icon={data.icon} id={data.id} />
-                                    )
-                                })
-                            }
-                        </div>
-
-                        <div className="flex flex-col xl:flex-row gap-8 mt-10">
-                            <div className="overflow-y-auto flex-1 bg-white rounded-lg shadow-sm">
-                                <h1 className="text-primary text-xl py-4 font-medium px-6 border-b-2 border-gray-200">Recent Scans</h1>
-                                <DashboardTable data={scansData} />
-                            </div>
-                        </div>
-                    </> :
-                    <>
-                        <div className="mb-10">
-                            <div className="flex justify-between items-center w-full mt-6">
-                                <h1 className="font-bold text-primary text-2xl leading-tight">Profile</h1>
-                                <div className="flex items-center gap-2">
-                                    <Link to={`/user/profile/update`} state={{ user }} className="text-grey-lighter py-2 px-2 rounded-md bg-blue-600 hover:bg-blue-700">
-                                        <MdOutlineEdit size={22} color='white' />
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="flex flex-col sm:flex-row gap-10">
-                            <div className="flex justify-center">
+                errorData ?
+                    <h1>{errorData.error}</h1> :
+                    user.role === 'doctor' ?
+                        <>
+                            <div className="grid grid-cols-1 gap-4 ss:grid-cols-2 mt-6">
                                 {
-                                    user.img ?
-                                        <div className="w-40 h-40 border-primary border-2 rounded-full p-1">
-                                            <img src={user.img} className='rounded-full' alt={`${user.firstName} ${user.lastName}`} />
-                                        </div> :
-                                        <HiUser size={200} color='#07074D' className='rounded-full border-primary border-2 p-3' />
+                                    dashboardCardsInfo.map((data, index) => {
+
+                                        return (
+                                            <OverviewCard key={index} count={data.count} title={data.title} icon={data.icon} id={data.id} />
+                                        )
+                                    })
                                 }
                             </div>
 
-                            <fieldset className="w-full border-2 border-gray-300 rounded-md px-6 py-4 grid grid-cols-1 ss:grid-cols-2 md:grid-cols-3 gap-3">
-                                <legend className='font-semibold text-primary px-1'>User Details</legend>
+                            <div className="flex flex-col xl:flex-row gap-8 mt-10">
+                                <div className="overflow-y-auto flex-1 bg-white rounded-lg shadow-sm">
+                                    <h1 className="text-primary text-xl py-4 font-medium px-6 border-b-2 border-gray-200">Recent Scans</h1>
+                                    <DashboardTable data={doctorData.recentScans} />
+                                </div>
+                            </div>
+                        </> :
+                        <>
+                            <div className="mb-10">
+                                <div className="flex justify-between items-center w-full mt-6">
+                                    <h1 className="font-bold text-primary text-2xl leading-tight">Profile</h1>
+                                    <div className="flex items-center gap-2">
+                                        <Link to={`/user/profile/update`} state={{ user }} className="text-grey-lighter py-2 px-2 rounded-md bg-blue-600 hover:bg-blue-700">
+                                            <MdOutlineEdit size={22} color='white' />
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
 
-                                <div className="">
-                                    <h4 className="block text-base font-semibold text-[#07074D]">
-                                        First Name
-                                    </h4>
-                                    <p className="w-full text-base font-medium text-[#6B7280]">
-                                        {user.firstName}
-                                    </p>
+                            <div className="flex flex-col sm:flex-row gap-10">
+                                <div className="flex justify-center">
+                                    {
+                                        user.img ?
+                                            <div className="w-40 h-40 border-primary border-2 rounded-full p-1">
+                                                <img src={nurseData.img} className='rounded-full' alt={`${nurseData.firstName} ${nurseData.lastName}`} />
+                                            </div> :
+                                            <HiUser size={200} color='#07074D' className='rounded-full border-primary border-2 p-3' />
+                                    }
                                 </div>
-                                <div className="">
-                                    <h4 className="block text-base font-semibold text-[#07074D]">
-                                        Last Name
-                                    </h4>
-                                    <p className="w-full text-base font-medium text-[#6B7280]">
-                                        {user.lastName}
-                                    </p>
-                                </div>
-                                <div className="">
-                                    <h4 className="block text-base font-semibold text-[#07074D]">
-                                        Email
-                                    </h4>
-                                    <p className="w-full text-base font-medium text-[#6B7280]">
-                                        {user.email}
-                                    </p>
-                                </div>
-                                <div className="">
-                                    <h4 className=" block text-base font-semibold text-[#07074D]">
-                                        Specialty
-                                    </h4>
-                                    <p className="w-full text-base font-medium text-[#6B7280]">
-                                        {user.specialty}
-                                    </p>
-                                </div>
-                                <div className="">
-                                    <h4 className=" block text-base font-semibold text-[#07074D]">
-                                        Role
-                                    </h4>
-                                    <p className="w-full text-base font-medium text-[#6B7280] capitalize">
-                                        {user.role}
-                                    </p>
-                                </div>
-                            </fieldset>
-                        </div>
-                    </>
+
+                                <fieldset className="w-full border-2 border-gray-300 rounded-md px-6 py-4 grid grid-cols-1 ss:grid-cols-2 md:grid-cols-3 gap-3">
+                                    <legend className='font-semibold text-primary px-1'>User Details</legend>
+
+                                    <div className="">
+                                        <h4 className="block text-base font-semibold text-[#07074D]">
+                                            ID
+                                        </h4>
+                                        <p className="w-full text-base font-medium text-[#6B7280]">
+                                            {nurseData.user.id}
+                                        </p>
+                                    </div>
+                                    <div className="">
+                                        <h4 className="block text-base font-semibold text-[#07074D]">
+                                            Email
+                                        </h4>
+                                        <p className="w-full text-base font-medium text-[#6B7280]">
+                                            {nurseData.user.email}
+                                        </p>
+                                    </div>
+                                    <div className="">
+                                        <h4 className=" block text-base font-semibold text-[#07074D]">
+                                            Role
+                                        </h4>
+                                        <p className="w-full text-base font-medium text-[#6B7280] capitalize">
+                                            {nurseData.user.role}
+                                        </p>
+                                    </div>
+                                    <div className="">
+                                        <h4 className="block text-base font-semibold text-[#07074D]">
+                                            First Name
+                                        </h4>
+                                        <p className="w-full text-base font-medium text-[#6B7280]">
+                                            {nurseData.firstName}
+                                        </p>
+                                    </div>
+                                    <div className="">
+                                        <h4 className="block text-base font-semibold text-[#07074D]">
+                                            Last Name
+                                        </h4>
+                                        <p className="w-full text-base font-medium text-[#6B7280]">
+                                            {nurseData.lastName}
+                                        </p>
+                                    </div>
+                                    <div className="">
+                                        <h4 className=" block text-base font-semibold text-[#07074D]">
+                                            Specialty
+                                        </h4>
+                                        <p className="w-full text-base font-medium text-[#6B7280]">
+                                            {nurseData.specialty}
+                                        </p>
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </>
+
             }
-
-
-
         </section>
     )
 }
--- hospital-scan-archive-system-FE/src/pages/user/UserForm.jsx
+++ hospital-scan-archive-system-FE/src/pages/user/UserForm.jsx
@@ -1,14 +1,113 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useRef, useState } from 'react';
+import { Link, useLocation, useLoaderData, useNavigate } from 'react-router-dom';
+import { requireAuth } from '../../utils';
+import { getUser, updateUserProfile } from '../../api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export async function loader({ request }) {
+    await requireAuth(request);
+    const authenticatedUser = JSON.parse(localStorage.getItem('user'));
+    const user = await getUser(authenticatedUser.userId, request);
+
+    if (user.error || user.message) {
+        return {
+            error: `${user.error ?? ''} ${user.message}`
+        }
+    }
+
+    return user;
+}
 
 const UserForm = () => {
     const { state } = useLocation();
-    const { user } = state;
+    const user = useLoaderData();
+    const navigate = useNavigate()
+    const fileRef = useRef(null);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [formData, setFormData] = useState({
+        firstName: user.firstName ?? '',
+        lastName: user.lastName ?? '',
+        gender: user.gender ?? '',
+        speciality: user.speciality ?? '',
+        phoneNumber: user.phoneNumber ?? '',
+        email: user.email ?? '',
+        file: '',
+    });
 
-    // console.log(state);
+    function handleChange(e) {
+        const { name, value, type, checked } = e.target;
+        const elementValue = type === 'checkbox' ? checked : value;
+
+        setFormData(prev => ({
+            ...prev,
+            [name]: elementValue
+        }));
+    }
+
+    function browseImage(e) {
+        e.preventDefault();
+
+        fileRef.current?.click();
+    }
+
+    function handleImageSelect(e) {
+        e.preventDefault();
+
+        const file = e.target.files[0];
+
+        if (file) {
+            setSelectedImage(file);
+        }
+
+    }
+
+    async function updateProfile(e) {
+        e.preventDefault();
+
+        const profileData = new FormData();
+
+        for (const [prop, value] of Object.entries(formData)) {
+            profileData.append(prop, String(value));
+        }
+
+        profileData.delete('file');
+        profileData.append('file', selectedImage ? selectedImage : '');
+
+        const userResponse = await updateUserProfile(user.id, profileData);
+
+        if (userResponse.unAuthorize) {
+            const pathname = location.pathname;
+            navigate(`/?message=Please log in to continue&redirectTo=${pathname}`);
+        }
+
+        if (userResponse.error || userResponse.message) {
+            toast.error(`${userResponse.error} ${userResponse.message}`, {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 2000,
+            });
+
+            return {
+                error: userResponse.error
+            }
+        }
+
+        toast.success(`User successfully updated!`, {
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 2000,
+        });
+
+        setTimeout(() => {
+            if (user.role === 'doctor') {
+                navigate(`/user/profile`);
+            }
+            navigate(`/user`);
+        }, 3000);
+    }
 
     return (
         <div className="flex flex-col pt-6 font-poppins">
+            <ToastContainer />
             <div className="pb-6">
                 <nav aria-label="breadcrumb">
                     <ol className="flex space-x-2">
@@ -25,20 +124,21 @@ const UserForm = () => {
             </div>
 
             <div className="mx-auto w-full">
-                <form>
+                <form onSubmit={updateProfile}>
                     <div className="grid grid-cols-1 xs:grid-cols-2 gap-x-3 gap-y-6">
                         <div className="">
                             <label
                                 htmlFor='email'
                                 className="mb-1 block text-base font-medium text-[#07074D]"
                             >
-                                Email <span className="text-red-600">*</span>
+                                Email
                             </label>
                             <input
                                 type="email"
                                 name="email"
                                 id="email"
-                                defaultValue={user.email}
+                                value={formData.email}
+                                onChange={handleChange}
                                 placeholder="Email"
                                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                                 required
@@ -47,32 +147,17 @@ const UserForm = () => {
 
                         <div className="">
                             <label
-                                htmlFor="password"
+                                htmlFor='firstName'
                                 className="mb-1 block text-base font-medium text-[#07074D]"
                             >
-                                Password <span className="text-red-600">*</span>
-                            </label>
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder="Password"
-                                className="w-full rounded-s-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                                required
-                            />
-                        </div>
-                        <div className="">
-                            <label
-                                htmlFor='fName'
-                                className="mb-1 block text-base font-medium text-[#07074D]"
-                            >
-                                First Name <span className="text-red-600">*</span>
+                                First Name
                             </label>
                             <input
                                 type="text"
-                                name="fName"
-                                id="fName"
-                                defaultValue={user.firstName}
+                                name="firstName"
+                                id="firstName"
+                                value={formData.firstName}
+                                onChange={handleChange}
                                 placeholder="First Name"
                                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                                 required
@@ -83,13 +168,14 @@ const UserForm = () => {
                                 htmlFor="lastName"
                                 className="mb-1 block text-base font-medium text-[#07074D]"
                             >
-                                Last Name <span className="text-red-600">*</span>
+                                Last Name
                             </label>
                             <input
                                 type="text"
                                 name="lastName"
                                 id="lastName"
-                                defaultValue={user.lastName}
+                                value={formData.lastName}
+                                onChange={handleChange}
                                 placeholder="Last Name"
                                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                                 required
@@ -97,16 +183,17 @@ const UserForm = () => {
                         </div>
                         <div className="">
                             <label
-                                htmlFor='specialty'
+                                htmlFor='speciality'
                                 className="mb-1 block text-base font-medium text-[#07074D]"
                             >
-                                Specialty <span className="text-red-600">*</span>
+                                Specialty
                             </label>
                             <input
                                 type="text"
-                                name="specialty"
-                                id="specialty"
-                                defaultValue={user.specialty}
+                                name="speciality"
+                                id="speciality"
+                                value={formData.speciality}
+                                onChange={handleChange}
                                 placeholder="Specialty"
                                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                                 required
@@ -114,25 +201,70 @@ const UserForm = () => {
                         </div>
                         <div className="">
                             <label
-                                htmlFor="profileImage"
+                                htmlFor='phoneNumber'
                                 className="mb-1 block text-base font-medium text-[#07074D]"
+                            >
+                                Phone Number
+                            </label>
+                            <input
+                                type="text"
+                                name="phoneNumber"
+                                id="phoneNumber"
+                                value={formData.phoneNumber}
+                                onChange={handleChange}
+                                placeholder="08034670912"
+                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                required
+                            />
+                        </div>
+                        <div className="">
+                            <label
+                                htmlFor="gender"
+                                className="mb-1 block text-base font-medium text-[#07074D]"
+                            >
+                                Gender
+                            </label>
+                            <select
+                                name="gender"
+                                id="gender"
+                                value={formData.gender}
+                                onChange={handleChange}
+                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
+                                <option value="">--Select an option--</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="others">Others</option>
+                            </select>
+                        </div>
+                        <div className="self-end">
+                            <label
+                                htmlFor="file"
+                                className="mb-1 block text-base font-medium text-[#07074D] sr-only"
                             >
                                 Profile Image
                             </label>
                             <input
                                 type="file"
-                                name="profileImage"
-                                id="profileImage"
-                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                name="file"
+                                id="file"
+                                ref={fileRef}
+                                onChange={handleImageSelect}
+                                placeholder="Next of Kin"
+                                className="w-full rounded-s-md h-[50px] border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md hidden"
                             />
+                            <div className="flex items-end gap-4">
+                                <button onClick={browseImage} className="hover:shadow-form rounded-md bg-[#6A64F1] hover:bg-[#5f58f1] py-3 px-8 text-center text-base font-semibold text-white outline-none">{selectedImage ? 'Change Profile Image' : 'Browse Profile Image'}</button>
+                                <p className="font-poppins font-medium text-lg">{selectedImage ? selectedImage.name : 'No file chosen'}</p>
+                            </div>
                         </div>
                     </div>
 
                     <div className='flex justify-end mt-6'>
                         <button
+                            type='submit'
                             className="hover:shadow-form rounded-md bg-[#6A64F1] hover:bg-[#5f58f1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
                         >
-                            Create User
+                            Update Profile
                         </button>
                     </div>
                 </form>
