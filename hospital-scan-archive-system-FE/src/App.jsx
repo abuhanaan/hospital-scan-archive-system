ca17bef76fbba032e5638e2fdf9713a0585d910f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-01-14 03:20:46 +0100

    Adjust routes & add loaders to auth routes

--- hospital-scan-archive-system-FE/src/App.jsx
+++ hospital-scan-archive-system-FE/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
 import { AdminLayout, UserLayout, AuthLayout, Layout } from './components';
-import { Home, AdminDashboard, UsersList, UserView, UserForm, PatientView, PatientsList, PatientForm, ScanForm, ScansList, ScanView, UserDashboard, UserPatientsList, UserPatientView, UserScansList, UserScanView, UserScanForm, UserProfileView, UserProfileForm, loginLoader, adminDashboardLoader, usersListLoader, userViewLoader, patientViewLoader, patientsListLoader, scanViewLoader, scansListLoader, userDashboardLoader, userPatientsListLoader, userPatientViewLoader, userScansListLoader, userScanViewLoader, userScanFormLoader, userProfileViewLoader, loginAction, userCreateAction } from './pages';
+import { Home, AdminPasswordUpdate, PasswordUpdate, AdminDashboard, UsersList, UserView, UserForm, PatientView, PatientsList, PatientForm, ScanForm, ScansList, ScanView, UserDashboard, UserPatientForm, UserPatientsList, UserPatientView, UserScansList, UserScanView, UserScanForm, UserProfileView, UserProfileForm, loginLoader, adminDashboardLoader, usersListLoader, userViewLoader, patientViewLoader, patientsListLoader, scanViewLoader, scansListLoader, userDashboardLoader, userProfileUpdateLoader, userPatientsListLoader, userPatientViewLoader, userScansListLoader, userScanViewLoader, userProfileViewLoader, loginAction, userCreateAction } from './pages';
 import { createBrowserRouter, createRoutesFromChildren, RouterProvider, Route } from 'react-router-dom';
+import { requireAuth } from './utils';
 
 const router = createBrowserRouter(createRoutesFromChildren(
     <Route path='/' element={<Layout />}>
@@ -9,32 +10,35 @@ const router = createBrowserRouter(createRoutesFromChildren(
         {/* <Route element={<AuthLayout />}> */}
         <Route path='admin' element={<AdminLayout />}>
             <Route index loader={adminDashboardLoader} element={<AdminDashboard />} />
+            <Route path='change-password'  loader={async ({request}) => await requireAuth(request)} element={<AdminPasswordUpdate />} />
 
             <Route path='users' loader={usersListLoader} element={<UsersList />} />
             <Route path='users/:id' loader={userViewLoader} element={<UserView />} />
-            <Route path='users/create-user' element={<UserForm />} />
+            <Route path='users/create-user' loader={async ({request}) => await requireAuth(request)} element={<UserForm />} />
 
             <Route path='patients' loader={patientsListLoader} element={<PatientsList />} />
             <Route path='patients/:id' loader={patientViewLoader} element={<PatientView />} />
-            <Route path='patients/create-patient' element={<PatientForm />} />
+            <Route path='patients/create-patient' loader={async ({request}) => await requireAuth(request)} element={<PatientForm />} />
 
             <Route path='scans' loader={scansListLoader} element={<ScansList />} />
             <Route path='scans/:id' loader={scanViewLoader} element={<ScanView />} />
-            <Route path='scans/create-scan' element={<ScanForm />} />
+            <Route path='scans/create-scan'  loader={async ({request}) => await requireAuth(request)} element={<ScanForm />} />
         </Route>
 
         <Route path='user' element={<UserLayout />}>
             <Route index loader={userDashboardLoader} element={<UserDashboard />} />
 
             <Route path='profile' loader={userProfileViewLoader} element={<UserProfileView />} />
-            <Route path='profile/update' element={<UserProfileForm />} />
+            <Route path='profile/update' loader={userProfileUpdateLoader} element={<UserProfileForm />} />
+            <Route path='change-password'  loader={async ({request}) => await requireAuth(request)} element={<PasswordUpdate />} />
 
             <Route path='patients' loader={userPatientsListLoader} element={<UserPatientsList />} />
             <Route path='patients/:id' loader={userPatientViewLoader} element={<UserPatientView />} />
+            <Route path='patients/create-patient'  loader={async ({request}) => await requireAuth(request)} element={<UserPatientForm />} />
 
             <Route path='scans' loader={userScansListLoader} element={<UserScansList />} />
             <Route path='scans/:id' loader={userScanViewLoader} element={<UserScanView />} />
-            <Route path='scans/create-scan' loader={userScanFormLoader} element={<UserScanForm />} />
+            <Route path='scans/create-scan'  loader={async ({request}) => await requireAuth(request)} element={<UserScanForm />} />
         </Route>
         {/* </Route> */}
     </Route>
